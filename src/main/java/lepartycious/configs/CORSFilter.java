24627aad4c6d1681f3f72014cc7b72f3ac68f6d3
Mohit <mohitsingla2256@gmail.com>
2016-01-13 23:20:58 +0530

    Added filter to set response headers

---
+++ src/main/java/lepartycious/configs/CORSFilter.java
@@ -0,0 +1,42 @@
+package lepartycious.configs;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter{
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse response1=(HttpServletResponse) response;
+
+	    response1.setHeader("Access-Control-Allow-Origin", "*");
+	    response1.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+	    response1.setHeader("Access-Control-Max-Age", "3600");
+	    response1.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization,x-requested-with");
+	    chain.doFilter(request, response1);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+
+}
--- src/main/java/lepartycious/controllers/HomeController.java
+++ src/main/java/lepartycious/controllers/HomeController.java
@@ -1,25 +1,55 @@
 package lepartycious.controllers;
 
+import lepartycious.dtos.requestDTOs.HomeRequestDTO;
+import lepartycious.dtos.responseDTOs.HomeResponseDTO;
 import lepartycious.models.User;
 import lepartycious.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/user")
+@RequestMapping(value="/api/rest/v1/user")
 public class HomeController {
 	
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value="/getUser/{username}", method=RequestMethod.GET)
-	public String delete(@PathVariable String username) {
-		User user = userService.loadUserByUsername(username);
-		return user.getName();
+	@RequestMapping(method=RequestMethod.POST)
+	public String saveUser(@RequestBody HomeRequestDTO homeDTO) {
+		System.out.println("User Saved");
+		return "save";
+	}
+	
+	@RequestMapping(value= "/{id}", method=RequestMethod.DELETE)
+	public String deleteUser(@PathVariable Long id) {
+		System.out.println("User Deleted");
+		return "delete";
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT)
+	public String updateUser(@RequestBody HomeRequestDTO homeDTO) {
+		System.out.println("User Updated");
+		return "update";
+	}
+	
+	@RequestMapping(value= "/{id}", method=RequestMethod.GET)
+	public HomeResponseDTO getUserById(@PathVariable Long id) {
+		System.out.println("User Get By Id");
+		HomeResponseDTO homeResponseDTO = new HomeResponseDTO();
+		homeResponseDTO.setName("Mohit");
+		return homeResponseDTO;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String getAllUsers() {
+		System.out.println("User Get All");
+		return "getAllUsers";
 	}
 
 }
