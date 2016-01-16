0fd92f0a613811af6a4fcf90e67fd219fc69cc41
Mohit <mohitsingla2256@gmail.com>
2016-01-16 21:54:06 +0530

    Reverting spring secutiry code

--- src/main/java/lepartycious/services/currentuser/CurrentUserService.java
+++
@@ -1,9 +0,0 @@
-package lepartycious.services.currentuser;
-
-import lepartycious.models.CurrentUser;
-
-public interface CurrentUserService {
-
-    boolean canAccessUser(CurrentUser currentUser, Long userId);
-
-}
