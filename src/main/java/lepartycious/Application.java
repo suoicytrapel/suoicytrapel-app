8a4d0142073fd3e0028643cc975c8fb62a02dd60
Mohit <mohitsingla2256@gmail.com>
2016-02-11 00:03:57 +0530

    Tomcat deployment changes

--- src/main/java/lepartycious/Application.java
+++ src/main/java/lepartycious/Application.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
@@ -18,5 +20,9 @@ public class Application {
             System.out.println(beanName);
         }
     }
+    
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+     }
 
 }
