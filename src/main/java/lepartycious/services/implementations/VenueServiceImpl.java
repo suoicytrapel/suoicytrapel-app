887e9ccbf43ce18eb5081881dc5c8ec84a1b72e1
Ankit Bansal <anku171@gmail.com>
2016-07-14 23:01:38 +0530

    committing

--- src/main/java/lepartycious/services/implementations/VenueServiceImpl.java
+++ src/main/java/lepartycious/services/implementations/VenueServiceImpl.java
@@ -100,10 +100,14 @@ public class VenueServiceImpl implements VenueService {
 			SearchResponseDTO searchResponseDTO = new SearchResponseDTO();
 			searchResponseDTO.setName(venue.getName());
 			searchResponseDTO.setLocality(venue.getLocality().getDescription());
-			searchResponseDTO.setStartingPrice(venue.getStartingPrice());
 			searchResponseDTO.setMainImagerURL(venue.getAttachments().get(0).getImageURL());
 			searchResponseDTO.setMaxCapacity(venue.getMaxCapacity());
 			searchResponseDTO.setMinCapacity(venue.getMinCapacity());
+			for(VenueAmenities venueAmenities : venue.getVenueamenities()){
+				if(StringUtils.isNotEmpty(venueAmenities.getMinVegCost())){
+					searchResponseDTO.setStartingPrice(venueAmenities.getMinVegCost());
+				}
+			}
 			searchResponseDTOList.add(searchResponseDTO);
 		}
 	}
